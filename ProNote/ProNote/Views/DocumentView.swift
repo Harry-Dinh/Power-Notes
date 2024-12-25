4c834cb490950cff999c546dc22c974adbbde093
Harry Dinh <thongdinh2004@icloud.com>
2024-12-25 17:41:02 -0500

    Created DocumentView.swift
    This file will eventually replaces the PDFRenderer class to better work with SwiftUI as well as transfering values from SwiftUI views to this view.
    Other than that, it (should) work exactly the same as the PDFRenderer class so far. Need to test to see if it works or not

---
+++ ProNote/ProNote/Views/DocumentView.swift
@@ -0,0 +1,87 @@
+//
+//  DocumentView.swift
+//  ProNote
+//
+//  Created by Harry Dinh on 2024-12-25.
+//
+
+import SwiftUI
+import PDFKit
+import PencilKit
+
+struct DocumentView: UIViewRepresentable {
+    
+    private var pdfView = PDFView()
+    @Binding var document: PDFDocument?
+    
+    init(document: Binding<PDFDocument?>) {
+        self._document = document
+    }
+    
+    // MARK: - Make UIView Function
+    func makeUIView(context: Context) -> PDFView {
+        // Configure the PDF view
+        pdfView.delegate = context.coordinator
+        pdfView.autoScales = true
+        pdfView.displayMode = .singlePageContinuous
+        pdfView.displayDirection = .vertical
+        pdfView.displaysPageBreaks = true
+        pdfView.usePageViewController(false)
+        pdfView.isInMarkupMode = true
+        return pdfView
+    }
+    
+    // MARK: - Update UIView Function
+    func updateUIView(_ uiView: PDFView, context: Context) {
+        // No operations yet...
+    }
+    
+    // MARK: - Make Coordinator Function
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    // MARK: - Coordinator Class
+    class Coordinator: NSObject, PDFViewDelegate {
+        var parent: DocumentView
+        
+        init(_ parent: DocumentView) {
+            self.parent = parent
+        }
+    }
+}
+
+// MARK: - Coordinator Extensions
+extension DocumentView.Coordinator: PDFPageOverlayViewProvider, PKCanvasViewDelegate {
+    
+    // Set up and overlay the canvas view over each PDF file
+    func pdfView(_ view: PDFView, overlayViewFor page: PDFPage) -> UIView? {
+        let canvasView = PKCanvasView()
+        canvasView.delegate = self
+        
+        let pageBounds = page.bounds(for: .mediaBox)
+        canvasView.frame = pageBounds
+        
+        // Set the resolution so that the ink stroke doesn't look pixelated
+        canvasView.contentScaleFactor = view.maxScaleFactor * 2
+        
+        // Configure the canvas view
+        canvasView.drawingPolicy = .default
+        canvasView.backgroundColor = .clear
+        canvasView.overrideUserInterfaceStyle = .light
+        canvasView.isScrollEnabled = false
+        canvasView.isDrawingEnabled = true
+        canvasView.tool = PKInkingTool(.pen, color: .black, width: 5)
+        
+        // Add a double tap gesture recognizer to zoom the document to its width
+        let doubleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(doubleTapToZoomGesture))
+        doubleTapGestureRecognizer.numberOfTapsRequired = 2
+        canvasView.addGestureRecognizer(doubleTapGestureRecognizer)
+        
+        return canvasView
+    }
+    
+    @objc func doubleTapToZoomGesture() {
+        parent.pdfView.scaleFactor = parent.pdfView.scaleFactorForSizeToFit
+    }
+}
