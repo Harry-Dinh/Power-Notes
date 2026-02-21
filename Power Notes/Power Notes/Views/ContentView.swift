930d1283cafba67fb780624172bee9ad01f823ea
Harry Dinh <harrydinh1207@gmail.com>
2026-02-21 16:43:39 -0500

    Created project for the main app and organize the project directories

---
+++ Power Notes/Power Notes/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Power Notes
+//
+//  Created by Harry Dinh on 2026-02-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
