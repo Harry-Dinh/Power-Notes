2044b26be5cb40544dca6fa04e3353a187208391
Harry Dinh <harrydinh1207@gmail.com>
2026-05-13 16:28:23 -0400

    PWRNOTE-6: Project setup (#21)
    * Created project for the main app and organize the project directories
    
    * Finished setting up project directories for Power Notes

---
+++ Power Notes/Power Notes/Power_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Power_NotesApp.swift
+//  Power Notes
+//
+//  Created by Harry Dinh on 2026-02-21.
+//
+
+import SwiftUI
+
+@main
+struct Power_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
